2e2463e8e0e0053b27f281a84f51768c90089a1b
Username <87919916+AP0tato@users.noreply.github.com>
2024-10-31 15:56:51 -0400

    Update Robot.java

--- robot/Robot.java
+++ robot/Robot.java
@@ -2,61 +2,77 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-
 package frc.robot;
-
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import edu.wpi.first.wpilibj.Joystick;
+
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.Joystick;
 
-
-
-
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
 public class Robot extends TimedRobot {
-  
-  private VictorSPX driveLeft1 = new VictorSPX(1);
-  private VictorSPX driveRight1 = new VictorSPX(3);
-  private VictorSPX driveLeft2 = new VictorSPX(2);
-  private VictorSPX driveRight2 = new VictorSPX(4);
-  private Joystick joystick = new Joystick(0);
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
 
-
-  // Speed variables
-  double stopSpeed = 0.0;
-  double spinSpeed = 0.5; // Adjust this value for spin speed
-  double fastSpeed = 1.0; // Max forward speed
-
+  private VictorSPX right1, right2, left1, left2;
+  private Joystick stick;
+  private UsbCamera camera;
 
   @Override
-  public void robotInit() {
-   
-  }
-  
-  @Override
-  public void robotPeriodic() {
+  public void robotInit() 
+  {
+    right1 = new VictorSPX(3);
+    right2 = new VictorSPX(4);
+    left1 = new VictorSPX(1);
+    left2 = new VictorSPX(2);
 
+    stick = new Joystick(0);
   }
 
   @Override
-  public void autonomousInit() {
-    stop();
-  }
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
 
   @Override
   public void autonomousPeriodic() {}
 
   @Override
-  public void teleopInit() {
-    UsbCamera camera = CameraServer.startAutomaticCapture();
+  public void teleopInit() 
+  {
+    camera = CameraServer.startAutomaticCapture();
   }
 
   @Override
-  public void teleopPeriodic() {
+  public void teleopPeriodic() 
+  {
+    double x_val = stick.getRawAxis(1);
+    double y_val = stick.getRawAxis(0);
 
+    double speed_right = (x_val+y_val);
+    double speed_left = (x_val+y_val);
+
+    if(speed_left>1||speed_left<-1)
+      speed_left = (int)speed_left;
+
+    if(speed_right>1||speed_right<-1)
+      speed_right = (int)speed_right;
+
+    right1.set(ControlMode.PercentOutput, -speed_right);
+    right2.set(ControlMode.PercentOutput, -speed_right);
+    left1.set(ControlMode.PercentOutput, speed_left);
+    left2.set(ControlMode.PercentOutput, speed_left);
   }
 
   @Override
@@ -76,8 +92,4 @@ public class Robot extends TimedRobot {
 
   @Override
   public void simulationPeriodic() {}
-
-  public void stop() {
-   
-  }
 }
